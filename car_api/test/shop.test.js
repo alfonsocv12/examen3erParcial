380d028701ef01d187edcdac50713740eb0b2e71
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-11-24 10:05:33 -0700

    Se introducieron las pruebas

---
+++ car_api/test/shop.test.js
@@ -0,0 +1,9 @@
+import 'mocha'
+import { expect, assert } from 'chai'
+import "chai/register-should"
+import Shop from '../models/shopModel'
+// import initTest from './test_helper'
+import mongoose from 'mongoose'
+import shopController from '../controllers/shopController'
+
+console.log(shopController);
