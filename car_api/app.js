cb1dd406f2aa3a7e4af9992f6c054ce27912b53b
alfonsocv12 <alfonsocvu@gmail.com>
2019-11-23 18:59:01 -0700

    Primeros pasos api

---
+++ car_api/app.js
@@ -0,0 +1,29 @@
+const express = require("express"),
+    app = express(),
+    mongoose = require('mongoose')
+require('dotenv').config();
+
+app.listen(3000, function() {
+    console.log('listening on port 3000!');
+});
+
+/*set db*/
+mongoose.Promise = Promise
+async function run() {
+    await mongoose.connect(process.env.MONGODB_URI, {
+        autoReconnect: true,
+        reconnectTries: 1000000,
+        reconnectInterval: 3000,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+}
+run().catch(error => console.error(error))
+
+/*Plugings*/
+app.use(require('cors')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.use('/', require("./routes/landingRoutes"));
