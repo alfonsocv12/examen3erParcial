34d521a13b271e70e02b37e3ab619af7980c30e1
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-11-24 10:15:23 -0700

    Se agrego un testHelper para que inicie una nueva db con los datos de prueba

--- car_api/test/shop.test.js
+++ car_api/test/shop.test.js
@@ -1,9 +1,21 @@
-import 'mocha'
-import { expect, assert } from 'chai'
-import "chai/register-should"
-import Shop from '../models/shopModel'
-// import initTest from './test_helper'
-import mongoose from 'mongoose'
-import shopController from '../controllers/shopController'
+import 'mocha';
+import { expect, assert } from 'chai';
+import "chai/register-should";
+import Shop from '../models/shopModel';
+import initTest from './testHelper';
+import mongoose from 'mongoose';
+import { ShopController } from '../controllers/shopController';
 
-console.log(shopController);
+const shopController = new ShopController();
+
+describe('Shop',()=>{
+  describe('create',()=>{
+
+  });
+  describe('login',()=>{
+
+  });
+  describe('get all',()=>{
+    it('Should bring array all shops');
+  });
+})
