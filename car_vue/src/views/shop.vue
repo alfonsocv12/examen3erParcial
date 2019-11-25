f7b9e3e8dcc4a1e8213325718117e08eb85decf7
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-11-24 19:29:07 -0700

    file

---
+++ car_vue/src/views/shop.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  let storeModuleShop = "shop";
+
+  export default {
+    name:'login',
+    data(){
+      return{
+        email:null,
+        password:null
+      }
+    },
+    methods:{
+    
+    }
+  }
+
+</script>
